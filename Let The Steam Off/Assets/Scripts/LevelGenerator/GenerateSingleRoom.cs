c3805a48448e8e03176e257a5bbcd261edd20938
DESKTOP-JLN71CE\grzeg <grzegorzkielian99@gmail.com>
2022-04-10 19:58:23 +0200

    Adding a script that creates a single room consisting of the floor and walls.

---
+++ Let The Steam Off/Assets/Scripts/LevelGenerator/GenerateSingleRoom.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateSingleRoom : MonoBehaviour
+{
+
+    private List<Vector3> floorPositions = new List<Vector3>();
+    private List<Vector3> wallsPositions = new List<Vector3>();
+
+    private Hashtable floorContainer = new Hashtable();
+    private Hashtable wallsContainer = new Hashtable();
+
+    public GameObject floorBlockObject;
+    public GameObject wallBlockObject;
+    public GameObject objectToSpawn;
+    public GameObject player;
+
+    private Vector3 startPosition;
+
+    private int worldSizeX = 10;
+    private int worldSizeZ = 10;
+    private int wallsHeight = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid(floorBlockObject);
+        GenerateWalls(wallBlockObject);
+        CreateNodes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateGrid(GameObject spawnedObject)
+    {
+        for (int x = -worldSizeX; x < worldSizeX; x++)
+        {
+            for (int z = -worldSizeZ; z < worldSizeZ; z++)
+            {
+                Vector3 position = new Vector3(x * 1 + startPosition.x, 0, z * 1 + startPosition.z);
+                GameObject floor = Instantiate(spawnedObject, position, Quaternion.identity);
+                floorContainer.Add(position, floor);
+                floorPositions.Add(floor.transform.position);
+                floor.transform.SetParent(transform);
+            }
+        }
+    }
+
+    private void GenerateWalls(GameObject spawnedObject)
+    {
+        for (int x = 0; x < wallsHeight; x++)
+        {
+            for (int z = -worldSizeZ; z < worldSizeZ; z++)
+            {
+
+                CreateWall(spawnedObject, new Vector3(worldSizeX, x * 1, z + startPosition.z));
+                CreateWall(spawnedObject, new Vector3(-worldSizeX - 1, x * 1, z + startPosition.z));
+                CreateWall(spawnedObject, new Vector3(z + startPosition.x, x, worldSizeZ));
+                CreateWall(spawnedObject, new Vector3(z + startPosition.x, x, -worldSizeZ - 1));
+
+            }
+        }
+    }
+
+    private void CreateWall(GameObject spawnedObject, Vector3 newposition)
+    {
+        Vector3 position = newposition;
+        GameObject wall = Instantiate(spawnedObject, position, Quaternion.identity);
+        wallsContainer.Add(position, wall);
+        wallsPositions.Add(wall.transform.position);
+        wall.transform.SetParent(transform);
+    }
+
+    private void CreateNodes()
+    {
+
+    }
+}
