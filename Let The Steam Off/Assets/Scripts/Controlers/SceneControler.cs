19ebca702065431cebff60fae4c081929c9f2507
RolePoloGame <78389182+RolePoloGame@users.noreply.github.com>
2022-04-11 12:30:15 +0200

    Scene Manager + new Scene

---
+++ Let The Steam Off/Assets/Scripts/Controlers/SceneControler.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControler : MonoBehaviour
+{
+    public static SceneControler Load { 
+        get; 
+        private set; 
+    }
+
+    private void Awake()
+    {
+        Load = this;
+        Load.Scene(GameScenes.sc_MainMenu);
+    }
+
+    public float Progress = 0.0f;
+    public bool isDone = false;
+
+    private static GameScenes SceneToLoad;
+
+    public void Scene(GameScenes scene)
+    {
+        if (scene == GameScenes.sc_LoadScreen)
+        {
+            Coroutine coroutine = StartCoroutine(LoadAsync());
+            return;
+        }
+
+        SceneToLoad = scene;
+        SceneManager.LoadScene(GameScenes.sc_LoadScreen.ToString());
+    }
+
+    private IEnumerator LoadAsync()
+    {
+        Progress = 0.0f;
+        isDone = false;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneToLoad.ToString());
+
+        while (!operation.isDone)
+        {
+            Progress = Mathf.Clamp01(operation.progress / .9f);
+            yield return null;
+        }
+
+        isDone = true;
+        Progress = 1.0f;
+    }
+
+
+    public enum GameScenes
+    {
+        sc_MainMenu,
+        sc_MainScene,
+        sc_LoadScreen
+    }
+
+
+}
